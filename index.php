31538503d2c1f5d3982af07974825709cc582505
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-11-28 18:05:26 +0100

    SESSION issues

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php 
-            
     
+    session_start();
+
     include __DIR__ . "/functions.php";
     
     $length = $_GET["length"] ?? "";
     $password = createPassword($length);
-    
+    //header("Location: ./result.php");
+    $_SESSION["password"] = $password;
             
 ?>
 
@@ -19,7 +21,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-    <form action="index.php" method="GET">
+    <form action="result.php" method="GET">
 
         <label for="length">Lunghezza Password</label>
         <input type="text" name="length">
