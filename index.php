1958ddfca92dffc9953657d32046b0b27181b143
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-11-28 17:44:09 +0100

    password in one file

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
-<?php
+<?php 
+            
+    $characters =   [["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+                    ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+                    ["0","1","2","3","4","5","6","7","8","9"],
+                    ["!","£","$","%","&","/","(",")","="]];
 
-    function createPassword() {
+    $length = $_GET["length"] ?? "";
+    $password = [];
+    for($i = 0; $i<$length; $i++) {
 
-        $length = $_GET("length");
-        echo $length;
+        $type=rand(0, (count($characters)-1));
+        
+        $value = rand(0, (count($characters[$type])-1));
+
+        $password[] = $characters[$type][$value];
 
     }
 
-
+    $password = implode("", $password);
+    
+            
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +36,10 @@
 
         <label for="length">Lunghezza Password</label>
         <input type="text" name="length">
-        <button></button>
+        <button>CALCOLA</button>
     </form>
+    <h2>
+    <?php var_dump($password) ; ?>
+    </h2>
 </body>
 </html>
