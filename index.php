fa469dca6586476b00aab2ebddb8ea42cfbef402
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-11-28 14:15:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+    function createPassword() {
+
+        $length = $_GET("length");
+        echo $length;
+
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <form action="index.php" method="GET">
+
+        <label for="length">Lunghezza Password</label>
+        <input type="text" name="length">
+        <button></button>
+    </form>
+</body>
+</html>
